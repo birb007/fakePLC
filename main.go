ed631a5a6d90a325822f9e176c55d32f5c83cd48
birb007 <example@example.com>
2022-11-23 21:10:33 +0000

    update main.go to reflect changes

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 func main() {
     log.SetFlags(log.LstdFlags | log.Lshortfile)
     plc := s71200.New()
+    defer plc.Close()
 
-    request := []byte{0x3, 0x00, 0x00, 0x00, 0x01}
+    request := []byte{0x2b, 0x0e, 0x04, 0x01}
     plc.Process(request)
 }

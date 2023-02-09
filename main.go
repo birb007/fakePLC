33ceb545ad666eb0fe939030a15706e489fc6856
birb007 <example@example.com>
2023-02-09 14:42:53 +0000

    barebones service launch

--- main.go
+++ main.go
@@ -4,24 +4,7 @@ import (
     "context"
     "log"
     "fakePLC/plc/s71200"
-    "net"
 )
-
-func sendRequest(network, address string, request []byte) {
-    conn, err := net.Dial(network, address)
-    if err != nil {
-        panic(err)
-    }
-    defer conn.Close()
-    if _, err := conn.Write(request); err != nil {
-        panic(err)
-    }
-    // Block while waiting for request to process.
-    buf := make([]byte, 100)
-    if _, err := conn.Read(buf); err != nil {
-        panic(err)
-    }
-}
 
 func main() {
     log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -32,7 +15,6 @@ func main() {
     launchService(ctx, "tcp", "localhost:9999", plc.HandleMODBUSConn)
     defer cancel()
 
-    // Send request to ourselves.
-    request := []byte{0x2b, 0x0e, 0x04, 0x01}
-    sendRequest("tcp", "localhost:9999", request)
+    // Wait forever.
+    select{}
 }
